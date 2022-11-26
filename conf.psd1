d6085e191eca1c136641f94386909139cc8b6535
alhaos <52729006+alhaos@users.noreply.github.com>
2022-11-26 19:02:16 +0500

    fixed to parallel upload

--- conf.psd1
+++ conf.psd1
@@ -2,8 +2,10 @@
     Address = "test.rebex.net"
     Username = "demo"
     Password = "password"
+    SshHostKeyFingerprint = "ssh-rsa 3072 PqAcZt5RQU1FTMZIwhBdXe4ylqNflWOtGfNQ2ByWmyQ"
     DirectoryList = @(
-        "/pub/example/"
+        "d:\sftp\uploads_1",
+        "d:\sftp\uploads_2",
+        "d:\sftp\uploads_3"
     )
-    LocalDirecory = "d:\downloads"
 }
--- script.ps1
+++ script.ps1
@@ -1,24 +1,38 @@
-$conf = Import-PowerShellDataFile .\conf.psd1
+$conf = @{
+    Address = "172.86.123.205"
+    Username = "root"
+    Password = "6f227684d439D$"
+    SshHostKeyFingerprint = "ssh-rsa 3072 PqAcZt5RQU1FTMZIwhBdXe4ylqNflWOtGfNQ2ByWmyQ"
+    DirectoryList = @(
+        "d:\sftp\uploads_1",
+        "d:\sftp\uploads_2",
+        "d:\sftp\uploads_3"
+    )
+    RemotePath = "/"
+}
+
 Add-Type -Path .\lib\WinSCP-5.21.5-Automation\WinSCPnet.dll
+$ErrorActionPreference = 'stop'
+
+$PSNativeCommandUseErrorActionPreference
 
 $sessionOptions = New-Object WinSCP.SessionOptions -Property @{
     Protocol              = [WinSCP.Protocol]::Sftp
     HostName              = $conf.Address
     UserName              = $conf.Username
     Password              = $conf.Password
-    SshHostKeyFingerprint = "ssh-ed25519 255 d7Te2DHmvBNSWJNBWik2KbDTjmWtYHe2bvXTMM9lVg4"
+    SshHostKeyFingerprint = $conf.SshHostKeyFingerprint
 }
 
 $session = New-Object WinSCP.Session
+$session.Open($sessionOptions)
+$transferOptions = New-Object WinSCP.TransferOptions
+$transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
 
-foreach ($dir in $conf.DirectoryList) {
-    $session.Open($sessionOptions)
-    $transferOptions = New-Object WinSCP.TransferOptions
-    $transferOptions.TransferMode = [WinSCP.TransferMode]::Binary
-    $transferResult =
-    $session.GetFiles((Join-Path $dir "*"), $conf.LocalDirecory, $False, $transferOptions)
-    $transferResult.Check()
+workflow wf {
+    foreach -parallel ($dir in $conf.DirectoryList) {
+        $transferResult = $session.PutFiles($dir, "/", $false, $transferOptions)
+    }
 }
 
 $session.Dispose()
-
